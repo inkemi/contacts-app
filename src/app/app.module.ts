36d1032c7b45fdf36eb4d0226a6acf2111c1e55f
inkemi <mt.inkeroinen@gmail.com>
2017-11-01 10:02:13 +0200

    directory tree is composed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ContactListItemComponent } from './contact/contact-list/contact-list-item/contact-list-item.component';
+import { ContactListComponent } from './contact/contact-list/contact-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ContactListItemComponent,
+    ContactListComponent
   ],
   imports: [
     BrowserModule
